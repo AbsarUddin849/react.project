242d9df9f43112651ecfdd08a6e8966e4c1b04ad
absar uddin <absaruddin849@gmail.com>
2022-11-14 07:35:40 +0000

    erros outside of src need to be solved.

--- react-project/src/pages/Home.js
+++ react-project/src/pages/Home.js
@@ -1,48 +1,46 @@
 
-// import Feed from '../Component/Home/Feed/Feed';
+// import Feed from '../../Component/Home/Feed/Feed';
 // import LoginOption from 'react-project/src/component/Home/LoginOption/LoginOption';
-// //import Logo from 'react-project/src/component/Home/Logo/Logo';
-// import './Home.css';
+// import Logo from 'react-project/src/component/Home/Logo/Logo';
+//  import './Home.css';
 // // import Logo from '../../components/Home/Logo/Logo';
 // // import Feed from '../../components/Home/Feed/Feed';
 // // import LoginOption from '../../components/Home/LoginOption/LoginOption';
+
+function Home(){
+    return(
+        <div className="home">
+            <div className="leftContainer">Left</div>
+            <div className="centerContainer"> Center</div>
+            <div className="rightContainer">Right</div>
+        </div>
+    );
+}
+
+ export default Home;
+
+
+
+ 
+// import './Home.css';
+// import Logo from '../../components/Logo';
+// import Feed from '../../components/Feed';
+// import LoginOption from '../../components/LoginOption';
 
 // function Home(){
 //     return(
 //         <div className="home">
 //             <div className="leftContainer">
-//             {/* <Logo/> */}
+//                 <Logo />
 //             </div>
 //             <div className="centerContainer">
-//             <Feed/>
+//                 <Feed />
 //             </div>
 //             <div className="rightContainer">
-//             <LoginOption/>
+//                 <LoginOption />
 //             </div>
 //         </div>
 //     );
 // }
 
 // export default Home;
-import './Home.css';
-import Logo from '../../components/Logo';
-import Feed from '../../components/Feed';
-import LoginOption from '../../components/LoginOption';
-
-function Home(){
-    return(
-        <div className="home">
-            <div className="leftContainer">
-                <Logo />
-            </div>
-            <div className="centerContainer">
-                <Feed />
-            </div>
-            <div className="rightContainer">
-                <LoginOption />
-            </div>
-        </div>
-    );
-}
-
-export default Home;
