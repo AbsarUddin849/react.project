7f13a031c37b6a21482f0896f301ecd9b3f7b316
absar uddin <absaruddin849@gmail.com>
2022-11-14 21:24:36 +0000

    With folder not exist

---
+++ react-project/src/component/Home/loginOption/LoginOption.js
@@ -0,0 +1,13 @@
+
+
+
+// function LoginOption(){
+//     return <h4>Feed</h4>
+// }
+
+// export default LoginOption;
+function LoginOption(){
+    return <h4>LoginOption with folder</h4>
+}
+
+export default LoginOption;
---
+++ react-project/src/pages/Home/Home.js
@@ -0,0 +1,52 @@
+
+// import Feed from '../../Component/Home/Feed/Feed';
+// import LoginOption from 'react-project/src/component/Home/LoginOption/LoginOption';
+// import Logo from 'react-project/src/component/Home/Logo/Logo';
+import'./Home.css'
+import Logo from '../../component/Home/logo/Logo';
+  import Feed from '../../component/Home/feed/Feed';
+  import LoginOption from '../../component/Home/loginOption/LoginOption';
+
+function Home(){
+    return(
+        <div className="home">
+            <div className="leftContainer"> 
+             <Logo/> 
+            </div>
+            <div className="centerContainer"> 
+            <Feed/> 
+             </div>
+            <div className="rightContainer">
+            <LoginOption/> 
+             </div>
+        </div>
+    );
+}
+
+ export default Home;
+
+
+
+ 
+// import './Home.css';
+// import Logo from '../../components/Logo';
+// import Feed from '../../components/Feed';
+// import LoginOption from '../../components/LoginOption';
+
+// function Home(){
+//     return(
+//         <div className="home">
+//             <div className="leftContainer">
+//                 <Logo />
+//             </div>
+//             <div className="centerContainer">
+//                 <Feed />
+//             </div>
+//             <div className="rightContainer">
+//                 <LoginOption />
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Home;
